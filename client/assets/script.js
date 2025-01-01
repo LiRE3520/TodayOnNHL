25cfb572a741abf3ff50f1a51c95fcf4627c0ab9
Lewis Reed <lewisreed.352@gmail.com>
2025-01-01 17:19:03 +0000

    homepage loads with correct info. if data is changed, website is also updated

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,42 +1,43 @@
-document.getElementById("showStandings").addEventListener("click", function() {
+function viewStandings() {
     document.getElementById("standings").style.display = "block";
     document.getElementById("home").style.display = "none";
     document.getElementById("schedule").style.display = "none";
     getStandings();
-});
+}
 
-document.getElementById("showSchedule").addEventListener("click", function() {
+function viewSchedule() {
     document.getElementById("standings").style.display = "none";
-    document.getElementById("schedule").style.display = "block";
     document.getElementById("home").style.display = "none";
+    document.getElementById("schedule").style.display = "block";
     getSchedule();
-});
+}
 
-document.getElementById("title").addEventListener("click", function() {
+function viewHome() {
     document.getElementById("standings").style.display = "none";
-    document.getElementById("schedule").style.display = "none";
     document.getElementById("home").style.display = "block";
-});
-
-document.getElementById("seeTheStandings").addEventListener("click", function() {
-    document.getElementById("standings").style.display = "block";
-    document.getElementById("home").style.display = "none";
     document.getElementById("schedule").style.display = "none";
-    getStandings();
-});
+    getHome();
+}
 
-document.getElementById("seeTheSchedule").addEventListener("click", function() {
-    document.getElementById("standings").style.display = "none";
-    document.getElementById("home").style.display = "none";
-    document.getElementById("schedule").style.display = "block";
-    getSchedule();
-});
+function getHome() {
+    fetch("/api/home")
+        .then(resp => resp.json())
+        .then(data => {
+            document.getElementById("topTeamName").innerHTML = data.topTeam.name;
+            document.getElementById("topTeamLogo").innerHTML = `
+            <img src="${data.topTeam.logo}" class="img-fluid" height=190 width=190>`
+            document.getElementById("nextMatchTeams").innerHTML = `
+            ${data.nextMatch.team1} @ ${data.nextMatch.team2}`;
+            document.getElementById("nextMatchLogos").innerHTML = `
+            <img src="assets/logos/${data.nextMatch.team1}.svg" class="img-fluid" height=190 width=190>
+            <img src="assets/logos/${data.nextMatch.team2}.svg" class="img-fluid" height=190 width=190>`;
+        });
+}
 
 function getStandings() {
     fetch("/api/standings")
         .then(resp => resp.json())
         .then(teams => {
-            teams.sort((a, b) => a.position - b.position);
             const body = document.getElementById("standingsBody")
             body.innerHTML = "";
             for (let team of teams) {
@@ -55,7 +56,6 @@ function getSchedule() {
     fetch("/api/schedule")
         .then(resp => resp.json())
         .then(matches => {
-            matches.sort((a, b) => a.order - b.order);
             const section = document.getElementById("schedule")
             section.innerHTML = "";
             for (let match of matches) {
@@ -74,3 +74,12 @@ function getSchedule() {
             }
         });
 }
+
+document.addEventListener("DOMContentLoaded", viewHome);
+
+document.getElementById("showStandings").addEventListener("click", viewStandings);
+document.getElementById("showSchedule").addEventListener("click", viewSchedule);
+document.getElementById("title").addEventListener("click", viewHome);
+
+document.getElementById("seeTheStandings").addEventListener("click", viewStandings);
+document.getElementById("seeTheSchedule").addEventListener("click", viewSchedule);
--- server.js
+++ server.js
@@ -3,30 +3,28 @@ const fs = require('fs');
 
 const app = express()
 
-let teams = require('./data/teams.json')
-let matches = require('./data/matches.json')
-
 app.use(express.json())
 app.use(express.static("client"))
 
+app.get("/api/home", function(req,resp){
+    let teams = JSON.parse(fs.readFileSync('./data/teams.json', 'utf8'));
+    let matches = JSON.parse(fs.readFileSync('./data/matches.json', 'utf8'));
+    let topTeam = teams.find(team => team.position === 1);
+    let nextMatch = matches.find(match => match.order === 1);
+    resp.json({topTeam, nextMatch})
+})
+
 app.get("/api/standings", function(req,resp){
+    let teams = JSON.parse(fs.readFileSync('./data/teams.json', 'utf8'));
+    teams.sort((a, b) => a.position - b.position);
     resp.json(teams)
 })
 
 app.get("/api/schedule", function(req,resp){
+    let matches = JSON.parse(fs.readFileSync('./data/matches.json', 'utf8'));
+    matches.sort((a, b) => a.order - b.order);
     resp.json(matches)
 })
-
-
-
-
-
-
-
-
-
-
-
 
 
 
