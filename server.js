2f8bc6bd83c43e8d460d3db3297c2b6e39ac7366
Lewis Reed <Lewisreed.352@gmail.com>
2025-01-31 15:56:13 +0000

    changed server to start on localhost

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app'); // import API endpoints
 
-app.listen(8090, '10.247.157.33', () => { // start the server
-    console.log('Server running at: http://10.247.157.33:8090');
+app.listen(8090, () => { // start the server
+    console.log('Server running at: http://localhost:8090');
 });
